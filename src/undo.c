b394965bff534f7854943e6a4539c7f53ae8afe0
langel <knoxburry@gmail.com>
2023-06-20 23:43:05 -0400

    fixed undo after redo and debug cleanup

--- src/undo.c
+++ src/undo.c
@@ -44,21 +44,8 @@ int undo_forward() {
 	if (undo_head >= undo_max_steps) undo_head -= undo_max_steps;
 	pixel_struct pxl = pixel_new();
 	char * ptr = undo_log[undo_head];
-	printf("%zu\n", sizeof(ptr));
-	if (ptr == NULL) printf("ptr is null\n");
-	printf("before reconstruct\n");
-	for (int i = 0; i < 64; i++) {
-		printf("%2x ", ptr[0x12 + i]);
-	}
-	if (pixel_state_reconstruct(&pxl, ptr) == 1) printf("failed ident\n");
-	printf("\n");
-	printf("after reconstruct\n");
-	for (int i = 0; i < 64; i++) {
-		printf("%2x ", pxl.values[i]);
-	}
-	printf("%zu\n", sizeof(ptr));
-	printf("redo pixel state  x: %3d  y: %3d  h: %3d  w: %3d\n", pxl.rect.x, pxl.rect.y, pxl.rect.w, pxl.rect.h);
-	printf("redo size %d\n", pxl.size);
+	pixel_state_reconstruct(&pxl, ptr);
+	pixel_state_read(pxl, ptr);
 	pixel_state_plot(pxl);
 	undo_head = (undo_head + 1) % undo_max_steps;
 	return 1;
